2a9ef042f499efecf0e3c49e42e044a85e990830
Daniel Gonçalves <danielgoncalves229@gmail.com>
2023-06-06 17:20:49 +0100

    Add searchTopicByUser

--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -6,6 +6,16 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../user/schemas/user.schema';
 import { ConfigService } from '@nestjs/config';
 import { Topic } from './schemas/topic.schema';
+import { SearchTopicDto } from './dto/search-topic.dto';
+
+const apiKey = '9c00b654361b4202be900194835b8665';
+
+const searchGamesByID = async (ID) => {
+  const url = `https://api.rawg.io/api/games/${ID}?key=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 @Injectable()
 export class TopicService {
@@ -14,8 +24,7 @@ export class TopicService {
   async create(createTopicDto: CreateTopicDto) {
     const name = createTopicDto.name;
     const text = createTopicDto.text;
-    //const user_id = createTopicDto.user_id;
-    //const forum_id = createTopicDto.forum_id;
+
     if (name && text) {
       const topic = new this.TopicModel(createTopicDto);
       topic.save();
@@ -24,7 +33,32 @@ export class TopicService {
     else{
       return {status:400, message: "Fill all fields" };
     }
-   
+  }
+
+  async searchTopicByUser(searchTopicDto: SearchTopicDto) {
+    const username = searchTopicDto.username;
+
+    const user = await this.UserModel.findOne({ username: username });
+    if (!user) {
+      return { status: 203, message: "User not found" };
+    }
+
+    const topics = await this.TopicModel.find({ user_id: user._id }).select('_id name forum_id');
+    const images = [];
+
+    for (const topic of topics) {
+      const forumID = topic.forum_id;
+      const game = await searchGamesByID(forumID);
+      const { background_image: image } = game;
+      
+      if (image) {
+        images.push(image);
+      } else {
+        const defaultImage = "null";
+        images.push(defaultImage);
+      }
+    }
+    return { status: 200, message: { topics, images } };
   }
 
   findAll() {
