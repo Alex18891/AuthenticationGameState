0fdf6b9a51a3233410980a3cd6f3e02adabda75b
Daniel Gonçalves <danielgoncalves229@gmail.com>
2023-06-25 19:40:44 +0100

    Add GET Request to get games by release date

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -2,19 +2,34 @@ import { Injectable, flatten } from '@nestjs/common';
 import { SearchGameDto } from './dto/search-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 
-const apiKey = '9c00b654361b4202be900194835b8665';
+const apiKey = '50cf30549e214193975160de0871baf0';
 
 const searchGames = async (searchText) => {
   const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${searchText}&search_exact=true&ordering=-metacritic`
   return fetch(url).then((response) => response.json()).then((data) => data.results);
 };  
 
+const searchGamesByReleaseDate = async (order) => {
+  var url = null;
+  switch (order) {
+    case 1: 
+      url = `https://api.rawg.io/api/games?key=${apiKey}&dates=${getTodayMinusTwoMonths()},${getToday()}&ordering=-added&exclude_stores=4,8,9&exclude_additions=true`;
+      break;
+    case -1:
+      url = `https://api.rawg.io/api/games?key=${apiKey}&dates=${getToday()},${getTodayPlusOneYear()}&ordering=-added&exclude_stores=4,8,9&exclude_additions=true`;
+      break;
+  }
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
 const searchGamesByID = async (ID) => {
   const url = `https://api.rawg.io/api/games/${ID}?key=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
-};  
+};
 
 const allgames = async()=>{
   const url = `https://api.rawg.io/api/games?key=${apiKey}&ordering=-metacritic`
@@ -79,6 +94,26 @@ export class GameService {
     }
   }
 
+  async findByReleaseDate(ordering: String) {
+    var games = null;
+    if (ordering == "releasedate") {
+      games = await searchGamesByReleaseDate(1);
+    } else if (ordering == "-releasedate") {
+      games = await searchGamesByReleaseDate(-1);
+    } else return { status: 400, message: "Wrong filter"}
+    
+      const results = games.results.slice(0, 6); //limit to 6 games
+      const gameInformation = results.map(game => {
+        return {
+          id: game.id,
+          name: game.name,
+          released: game.released,
+          image: game.background_image
+        };
+      });
+      return { status: 200, message: gameInformation }
+  }
+
   findAll() {
     return `This action returns all game`;
   }
@@ -94,4 +129,39 @@ export class GameService {
   remove(id: number) {
     return `This action removes a #${id} game`;
   }
+}
+
+function getTodayMinusTwoMonths() {
+  var today = new Date();
+  today.setMonth(today.getMonth() - 2);
+
+  var year = today.getFullYear();
+  var month = String(today.getMonth() + 1).padStart(2, '0');
+  var day = String(today.getDate()).padStart(2, '0');
+
+  var formattedDate = year + '-' + month + '-' + day;
+  return formattedDate;
+}
+
+function getToday() {
+  var today = new Date();
+
+  var year = today.getFullYear();
+  var month = String(today.getMonth() + 1).padStart(2, '0');
+  var day = String(today.getDate()).padStart(2, '0');
+
+  var formattedDate = year + '-' + month + '-' + day;
+  return formattedDate;
+}
+
+function getTodayPlusOneYear() {
+  var today = new Date();
+  today.setFullYear(today.getFullYear() + 1);
+
+  var year = today.getFullYear();
+  var month = String(today.getMonth() + 1).padStart(2, '0');
+  var day = String(today.getDate()).padStart(2, '0');
+
+  var formattedDate = year + '-' + month + '-' + day;
+  return formattedDate;
 }
